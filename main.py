a0730fb0c91a774f5ca43df485b33c953ce80157
dima <petroff.dmit@gmail.com>
2021-12-27 18:16:29 +0300

    Добавлено стартовое окно

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ class Button(pg.sprite.Sprite):
     Класс кнопки, для удобной работы в меню
     """
     def __init__(self, ind: int, x: int, y: int, width_: int, height_: int, text: str, text_font: int,
-                 text_color=(255, 255, 255), back_color=(255, 255, 255)):
+                 text_color=(255, 255, 255), back_color=(0, 0, 0)):
         super().__init__()
         self.index = ind
         self.text_color = text_color
@@ -48,23 +48,31 @@ class Button(pg.sprite.Sprite):
         return self.rect.collidepoint(x, y)
 
 
-def set_start_win_btns() -> pg.sprite.Group:
+class Text(pg.sprite.Sprite):
+    def __init__(self, x: int, y: int, text: str, font_size: int, text_color=(255, 0, 0), back_color=(255, 255, 255)):
+        super().__init__()
+        self.text_color, self.back_color = text_color, back_color
+
+        font = pg.font.SysFont('Times New Roman', font_size)
+        self.text = font.render(text, True, text_color)
+
+        self.image = pg.Surface(self.text.get_size())
+        self.image.fill(back_color)
+
+        self.image.blit(self.text, ((self.image.get_width() - self.text.get_width()) // 2,
+                                    (self.image.get_height() - self.text.get_height()) // 2))
+
+        self.rect = pg.Rect(x, y, *self.image.get_size())
+
+
+def set_start_win_btns(y: int, win_width: int, btn_width: int, btn_height: int, btn_count: int,
+                       text_color: (int, int, int), back_color: (int, int, int)) -> pg.sprite.Group:
     """
     Генерация заданного количесва кнопок с опреденным шаблном
     """
-    dist_btween_start_btns = (start_win_width - start_win_btn_width * start_win_btns_count) // \
-                             (start_win_btns_count + 1)
-
-    start_win_btn_ind = 0
-    btns_sprites_ = pg.sprite.Group()
-    for x in range(start_win_btns_count):
-
-        button1 = Button(start_win_btn_ind, x * (start_win_btn_width + dist_btween_start_btns) + dist_btween_start_btns,
-                         300, start_win_btn_width, start_win_btn_height, f'{start_win_btn_ind + 1} уровень', 30,
-                         (0, 255, 0), (0, 0, 0))
-        btns_sprites_.add(button1)
-        start_win_btn_ind += 1
-
+    dist_btns = (win_width - btn_width * btn_count) // (btn_count + 1)
+    btns_sprites_ = pg.sprite.Group([Button(x, x * (btn_width + dist_btns) + dist_btns, y, btn_width, btn_height,
+                                            f'{x + 1} уровень', 30, text_color, back_color) for x in range(btn_count)])
     return btns_sprites_
 
 
@@ -76,7 +84,12 @@ if __name__ == '__main__':
     screen = pg.display.set_mode(start_win_size)
     pg.display.set_caption('Стартовое окно')
 
-    buttons_sprites = set_start_win_btns()
+    start_btns_sprites = set_start_win_btns(300, start_win_width, start_win_btn_width, start_win_btn_height,
+                                            start_win_btns_count, (0, 255, 0), (0, 0, 0))
+
+    game_title = Text(40, 40, 'The Best Game Ever', 30, (0, 255, 0), (0, 0, 0))
+    game_title.rect.x = (start_win_width - game_title.rect.width) // 2
+    text_sprites = pg.sprite.Group([game_title])
 
     while True:
         for event in pg.event.get():
@@ -85,14 +98,13 @@ if __name__ == '__main__':
                 exit()
             if event.type == pg.MOUSEBUTTONDOWN:
                 if event.button == 1:
-                    for btn in buttons_sprites.sprites():
+                    for btn in start_btns_sprites.sprites():
                         if btn.crossed(*event.pos):  # Здесь должна происходить загрузка уровня
+                            print(f'Запускается {btn.index + 1} уровень')
                             break
 
-        for sprite in buttons_sprites.sprites():
-            sprite.update()
-
-        buttons_sprites.draw(screen)
+        text_sprites.draw(screen)
+        start_btns_sprites.draw(screen)
 
         pg.display.flip()
-        screen.fill((255, 255, 255))
+        screen.fill((0, 0, 0))
